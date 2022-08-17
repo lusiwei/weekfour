32d3e0915f67e0c57186a80577dc7733e89bfea8
ackoo <ackoolu@gmail.com>
2022-08-17 16:18:36 +0800

    first commit

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -2,14 +2,14 @@ import { ethers } from "ethers";
 
 const NFTCard = ({ nft }) => {
   return (
-    <div className="w-1/4 flex flex-col">
+    <div className="w-1/4 flex flex-col h-1/2">
       <div className="rounded-md">
         <img
           className="object-cover h-128 w-full rounded-t-md"
           src={nft.media[0].gateway}
         ></img>
       </div>
-      <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+      <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110">
         <div className="">
           <h2 className="text-xl text-gray-800">{nft.title}</h2>
           <p className="text-gray-600">
@@ -22,7 +22,7 @@ const NFTCard = ({ nft }) => {
         </div>
 
         <div className="flex-grow mt-2">
-          <p className="text-gray-600 overflow-hidden text-ellipsis">{nft.description}</p>
+          <p className="text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap">{nft.description}</p>
         </div>
       </div>
     </div>
